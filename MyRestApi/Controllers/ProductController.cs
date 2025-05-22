2c204b03c6df28c36e54f3c487f50ff94eff2b98
T1mon1k <artemspivak2006@gmail.com>
2025-05-22 12:31:59 +0300

    Initial commit

---
+++ MyRestApi/Controllers/ProductController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyRestApi.Data;
+using MyRestApi.Models;
+
+namespace MyRestApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProductController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/product
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var products = await _context.Products.ToListAsync();
+            return Ok(products);
+        }
+
+        // GET: api/product/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+                return NotFound();
+            return Ok(product);
+        }
+
+        // POST: api/product
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] Product product)
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetById), new { id = product.Id }, product);
+        }
+    }
+}
---
+++ MyRestApi/Models/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace MyRestApi.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
